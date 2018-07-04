aae8ce918a340b7949f42b76dd4a1e0ab99315cd
Ezekiel Elin <ezekielelin@me.com>
2018-07-04 15:25:45 -0400

    Added Codable for identifiers

--- Sources/SwiftOSM/ObjectIdentifier.swift
+++ Sources/SwiftOSM/ObjectIdentifier.swift
@@ -16,3 +16,38 @@ public enum OSMIdentifier {
     case way(Int)
     case node(Int)
 }
+
+extension OSMIdentifier: Codable {
+    enum CodingKeys: CodingKey {
+        case type
+        case id
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let type = try container.decode(String.self, forKey: .type)
+        let id = try container.decode(Int.self, forKey: .id)
+        switch type {
+        case "node":
+            self = .node(id)
+        case "way":
+            self = .way(id)
+        default:
+            fatalError("\(type) isn't accounted for")
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case .node(let id):
+            try container.encode("node", forKey: .type)
+            try container.encode(id, forKey: .id)
+        case .way(let id):
+            try container.encode("way", forKey: .type)
+            try container.encode(id, forKey: .id)
+        }
+    }
+}
