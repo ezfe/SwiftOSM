77ff6478d2d6032e5eb46646e68a7c76c0950a22
Ezekiel Elin <ezekielelin@me.com>
2018-07-05 12:07:05 -0400

    Updated SWXMLHash

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.4.1"),
+        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.7.0"),
         .package(url: "https://github.com/davecom/SwiftPriorityQueue.git", from: "1.2.1")
     ],
     targets: [
