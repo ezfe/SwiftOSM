b1a9753e6c508e1aab1ff56e6203e1a04787d0a7
Ezekiel Elin <ezekielelin@me.com>
2019-04-16 14:55:08 -0400

    Fixed tests

--- Tests/SwiftOSMTests/SwiftOSMTests.swift
+++ Tests/SwiftOSMTests/SwiftOSMTests.swift
@@ -5,7 +5,7 @@ import SWXMLHash
 class SwiftOSMTests: XCTestCase {
     
     lazy var xmlData: XMLIndexer = {
-        let data = try! Data(contentsOf: URL(string: "https://f001.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_za1ea9c3fff6bfd94633d0b1e_f115aac9a06ddecc8_d20181110_m215634_c001_v0001105_t0051")!)
+        let data = try! Data(contentsOf: URL(string: "https://f001.backblazeb2.com/file/com-ezekielelin-LafayetteArtTour/map/lafayette_college.osm.xml")!)
         return SWXMLHash.parse(data)
     }()
     lazy var osm: OSM = {
@@ -38,24 +38,21 @@ class SwiftOSMTests: XCTestCase {
     }
     
     func testMotorwayNavigatable() {
-        let osm = OSM()
-        let way = OSMWay(id: -1, tags: [ "highway": "motorway" ], nodes: [], osm: osm)
+        let way = OSMWay(id: -1, tags: ["highway": "motorway"], nodes: [])
         
         XCTAssertTrue(way.access(for: .motor_vehicle))
         XCTAssertFalse(way.access(for: .foot))
     }
 
     func testFootwayNavigatable() {
-        let osm = OSM()
-        let way = OSMWay(id: -1, tags: [ "highway": "footway" ], nodes: [], osm: osm)
+        let way = OSMWay(id: -1, tags: [ "highway": "footway" ], nodes: [])
         
         XCTAssertFalse(way.access(for: .motor_vehicle))
         XCTAssertTrue(way.access(for: .foot))
     }
     
     func testMotorwayFootYesNavigatable() {
-        let osm = OSM()
-        let way = OSMWay(id: -1, tags: [ "highway": "motorway", "foot": "yes" ], nodes: [], osm: osm)
+        let way = OSMWay(id: -1, tags: [ "highway": "motorway", "foot": "yes" ], nodes: [])
         
         XCTAssertTrue(way.access(for: .motor_vehicle))
         XCTAssertTrue(way.access(for: .foot))
@@ -77,10 +74,10 @@ class SwiftOSMTests: XCTestCase {
             return
         }
 
-        self.measure { [weak self] in
-            osm.route(start: acopian.entrances, end: keefe.entrances)
-            osm.route(start: farinon.entrances, end: keefe.entrances)
-            osm.route(start: farinon.entrances, end: acopian.entrances)
+        self.measure {
+            _ = osm.route(start: acopian.entrances, end: keefe.entrances)
+            _ = osm.route(start: farinon.entrances, end: keefe.entrances)
+            _ = osm.route(start: farinon.entrances, end: acopian.entrances)
         }
     }
     
