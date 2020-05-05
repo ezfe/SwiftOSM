7ba439a87e0b81a7aa0a2ea58e680345c9f267e5
Ezekiel Elin <ezekielelin@me.com>
2020-05-05 15:42:09 -0400

    Updated packages

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftOSM",
+    name: "swift-osm",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftOSM",
-            targets: ["SwiftOSM"]),
+            targets: ["SwiftOSM"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
