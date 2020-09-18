1215af1d95dacfe31d6edd568e118f52b6524832
Ezekiel Elin <ezekielelin@me.com>
2020-09-18 14:16:51 -0400

    Verified working in Swift 5.3

--- Package.swift
+++ Package.swift
@@ -12,10 +12,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.1"),
-        .package(url: "https://github.com/davecom/SwiftPriorityQueue.git", from: "1.3.0")
+        .package(url: "https://github.com/davecom/SwiftPriorityQueue.git", from: "1.3.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
